0a3ac4e724c4533048a7d2066836e9d880bb1c6e
P4yBill <v4silismor@yahoo.com>
2020-06-25 22:39:38 +0300

    feat(Users api): Added users api routes(cud)

--- app/routes/api/users.py
+++ app/routes/api/users.py
@@ -1,4 +1,10 @@
-from flask import request
+from bson import json_util
+from flask import request, Response
+from ..db import db
+from ...login_manager import restricted
+from flask_login import login_required
+
+users = db['Users']
 
 
 class Users(object):
@@ -8,7 +14,79 @@ class Users(object):
 
     def register_user_routes(self, bp):
         @bp.route('/users', methods=['GET'])
+        @restricted(access_level="admin")
+        @login_required
         def get_user():
-            return {'response': 200, 'method': request.endpoint + request.method}
+            res_obj = {}
+            try:
+                users_retrieved = users.find({'category': 'user'}, {'email', 'name', 'category'})
+                users_as_list = list(users_retrieved)
+                res_obj["data"] = users_as_list
+                res_obj["error"] = ""
+                return Response(json_util.dumps(res_obj), status=200, mimetype="application/json")
+            except Exception as e:
+                res_obj["error"] = "Could not retrieve users"
+                res_obj["data"] = []
+                return Response(json_util.dumps(res_obj), status=500, mimetype='application/json')
 
+        @bp.route('/users', methods=['DELETE'])
+        @login_required
+        def delete_user():
+            res_obj = {"error": ""}
 
+            if request.is_json:
+                content = request.get_json()
+            else:
+                res_obj["error"] = "Bad Request"
+                return Response(json_util.dumps(res_obj), status=500, mimetype="application/json")
+
+            if "email" not in content:
+                res_obj["error"] = "Wrong params"
+                return Response(json_util.dumps(res_obj), status=500, mimetype="application/json")
+
+            user_mail = content["email"]
+
+            try:
+                user_to_delete = users.find_one({"email": user_mail})
+                if user_to_delete:
+                    users.delete_one(user_to_delete)
+
+                    res_obj["error"] = "OK"
+                    return Response(json_util.dumps(res_obj), status=200, mimetype="application/json")
+                else:
+                    res_obj["error"] = "We did not find the user"
+                    return Response(json_util.dumps(res_obj), status=500, mimetype='application/json')
+            except Exception as e:
+                res_obj["error"] = "User could not be deleted"
+                return Response(json_util.dumps(res_obj), status=500, mimetype='application/json')
+
+        @bp.route('/users', methods=['PUT'])
+        @restricted(access_level="admin")
+        @login_required
+        def update_user():
+            res_obj = {"error": ""}
+
+            if request.is_json:
+                content = request.get_json()
+            else:
+                res_obj["error"] = "Bad Request"
+                return Response(json_util.dumps(res_obj), status=500, mimetype="application/json")
+
+            if "email" not in content or "category" not in content:
+                res_obj["error"] = "Wrong params"
+                return Response(json_util.dumps(res_obj), status=500, mimetype="application/json")
+            user_mail = content["email"]
+            user_category = content["category"]
+            user_to_delete = users.find_one({"email" : user_mail})
+
+            if not user_to_delete:
+                res_obj["error"] = "Could not find the user"
+                return Response(json_util.dumps(res_obj), status=500, mimetype='application/json')
+
+            try:
+                users.update_one({"email": user_mail}, {"$set": {"category": user_category}})
+                res_obj["error"] = "OK"
+                return Response(json_util.dumps(res_obj), status=200, mimetype="application/json")
+            except Exception as e:
+                res_obj["error"] = "User could not be updated"
+                return Response(json_util.dumps(res_obj), status=500, mimetype='application/json')
