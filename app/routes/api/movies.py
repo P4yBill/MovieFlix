a043a0c3fecca5992d9e33cc57e1faa1f3fdfb7c
P4yBill <v4silismor@yahoo.com>
2020-06-26 20:58:22 +0300

    fix: Added some extra checks for the params

--- app/routes/api/movies.py
+++ app/routes/api/movies.py
@@ -2,6 +2,8 @@ from json import dumps
 
 from bson import ObjectId, json_util
 from flask import request, Response
+from werkzeug.wrappers import BaseRequest
+
 from ...login_manager import restricted
 from flask_login import login_required
 from ..db import db
@@ -33,26 +35,19 @@ class Movies(object):
                 res_obj = {"error": "OK"}
                 return Response(dumps(res_obj), status=200, mimetype="application/json")
             else:
-                res_obj = {"error": "You have to specify the title and at least on actor"}
+                res_obj = {"error": "Params are incorrect. Tip: you have to specify the title and at least one actor"}
                 return Response(dumps(res_obj), status=500, mimetype="application/json")
 
         @bp.route('/movies', methods=['GET'])
         @login_required
         def get_movies():
             res_obj = {"error": ""}
-            print(request.is_json)
-            if request.is_json:
-                content = request.get_json()
-            else:
-                res_obj["error"] = "Bad Request"
-                return Response(dumps(res_obj), status=500, mimetype="application/json")
-
-            movie_from_params = get_movie_params(content)
+            movie_from_params = get_movie_params()
 
             try:
                 movies_retrieved = movies.find(movie_from_params)
                 movies_as_list = list(movies_retrieved)
-                print(movies_as_list)
+
                 res_obj["error"] = "OK"
                 res_obj["data"] = movies_as_list
                 return Response(json_util.dumps(res_obj), status=200, mimetype="application/json")
@@ -82,14 +77,15 @@ class Movies(object):
 
             if movie_from_params:
                 try:
-                    movies.update_one({"_id": ObjectId(movie_id)}, {"$set": movie_from_params})
+                    res = movies.update_one({"_id": ObjectId(movie_id)}, {"$set": movie_from_params})
+
                     res_obj["error"] = "OK"
                     return Response(dumps(res_obj), status=200, mimetype="application/json")
                 except Exception as e:
                     res_obj["error"] = "Movie could not be updated"
                     return Response(dumps(res_obj), status=500, mimetype='application/json')
             else:
-                res_obj = {"error": "You have to specify the title and at least on actor"}
+                res_obj = {"error": "Params are incorrect. Please specify at least one valid actor and a title"}
                 return Response(dumps(res_obj), status=500, mimetype="application/json")
 
         @bp.route('/movies', methods=['DELETE'])
@@ -113,7 +109,8 @@ class Movies(object):
             try:
                 # find the older movies by title
                 movie_to_delete = movies.find({"title": movie_title}).sort("year", 1).limit(1)
-                if movie_to_delete.count() == 1:
+
+                if movie_to_delete.count() > 0:
                     # get the first movie from the cursor
                     movie_to_delete = movie_to_delete[0]
                     movies.delete_one(movie_to_delete)
@@ -134,8 +131,13 @@ def get_movie(movie, create=False):
 
     movie_to_send = {}
     if all(name in movie for name in required_fields):
+
         if isinstance(movie['actors'], list) and isinstance(movie['title'], str) and len(movie['actors']):
+            if not actors_params_correct(movie['actors']):
+                return
             for key in required_fields:
+                if movie[key] is '':
+                    return {}
                 movie_to_send[key] = movie[key]
             for key in fields:
                 if key in movie:
@@ -147,15 +149,33 @@ def get_movie(movie, create=False):
     return movie_to_send
 
 
-def get_movie_params(movie):
+def get_movie_params():
+    # actors will be a string not an array
     fields = ['title', 'year', 'actors']
 
     movie_to_send = {}
     for key in fields:
-        if key in movie:
-            movie_to_send[key] = movie[key]
+        get_param = request.args.get(key)
 
-    if "actors" in movie_to_send:
-        movie_to_send["actors"] = {"$all": movie_to_send["actors"]}
+        if get_param is not None:
+            if int(get_param):
+                get_param = int(get_param)
+            movie_to_send[key] = get_param
 
     return movie_to_send
+
+
+def actors_params_correct(arr: list):
+    try:
+        for val in arr:
+            if not str(val):
+                return False
+            if val == '':
+                return False
+    except ValueError:
+        return False
+
+    return True
+
+
+
